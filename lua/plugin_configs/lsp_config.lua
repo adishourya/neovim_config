43f8ca40bcd499a57a196f6ecc034555f3cb9d23
Aditya Shourya <shourya.aditya10@gmail.com>
2021-03-23 01:13:56 +0530

    Adding in a Readme file

--- lua/plugin_configs/lsp_config.lua
+++ lua/plugin_configs/lsp_config.lua
@@ -16,19 +16,19 @@ local on_attach = function(client, bufnr)
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
+    hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+    hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+    hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+    augroup lsp_document_highlight
+    autocmd! * <buffer>
+    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    augroup END
     ]], false)
   end
 end
 
--- Use a loop to conveniently both setup defined servers 
+-- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
 local servers = { "pyls", "sumneko_lua" }
 for _, lsp in ipairs(servers) do
@@ -37,26 +37,26 @@ for _, lsp in ipairs(servers) do
   -- nvim_lsp[lsp].setup { on_attach = require'completion'.on_attach }
 end
 
- local sumneko_root_path = "/home/adi/Documents/lang_servers/lua-language-server" 
- require'lspconfig'.sumneko_lua.setup{ 
-   cmd = {sumneko_root_path .. "/bin/Linux/lua-language-server", "-E", sumneko_root_path .. "/main.lua"},
+local sumneko_root_path = "/home/adi/Documents/lang_servers/lua-language-server"
+require'lspconfig'.sumneko_lua.setup{
+  cmd = {sumneko_root_path .. "/bin/Linux/lua-language-server", "-E", sumneko_root_path .. "/main.lua"},
   settings = {
-          Lua = {
-              runtime = {
-                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT',
-                  -- Setup your lua path
-                  path = vim.split(package.path, ';')
-              },
-              diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = {'vim'}
-              },
-              workspace = {
-                  -- Make the server aware of Neovim runtime files
-                  library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
-              }
-          }
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';')
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'}
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
       }
+    }
+  }
 
 }
