9a1f513f9f31604876ff44aacc351684b4a83f66
Aditya Shourya <shourya.aditya10@gmail.com>
2021-04-17 15:45:03 +0530

    Added Reindent func

--- lua/funcs.lua
+++ lua/funcs.lua
@@ -1,16 +1,35 @@
-
 -- ┌───────────────┐
 -- │Handy Functions│
 -- └───────────────┘
 --
 
-local lua_formatter = function()
+F = {}
 
+-- Dont chnage the name of the function
+--  references in keybinds.lua
+F.formatter = function()
   if vim.bo.filetype == "lua" then
+    -- vim.lsp.buf.formatting() doesn't work with lua for some reaseon
     vim.cmd("!lua ~/bin/luafmt.lua --f %")
-    print('Formatted !!')
   else
-    print('not a lua filetype')
+    vim.lsp.buf.formatting()
   end
 end
+
+F.reindent = function(tabsize)
+  if tabsize == nil then
+    -- dont know how to give default value
+    -- like in pythhon
+    tabsize =2
+  end
+  if vim.bo.filetype == "python" then
+    -- dont know why python has a hard on for multiple of 4
+    vim.bo.shiftwidth = 4
+  else
+    vim.bo.shiftwidth = tabsize
+    vim.cmd[[normal gg=G]]
+  end
+end
+
+return F
 
